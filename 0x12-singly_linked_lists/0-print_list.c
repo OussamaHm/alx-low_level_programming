0313800d34fe1395a11d31e3ff755355c2a1078f
OussamaHm <oussahm@gmail.com>
2023-05-01 16:29:32 -0700

    kokokoklpl

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+size_t print_list(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
